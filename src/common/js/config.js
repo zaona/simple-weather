316a1e6672fed6f6173dd777a4bd1b9796d5f88c
Zaona <3409473369@qq.com>
2026-03-19 14:01:45 +0800

    feat: 优化滚动进度条计算逻辑

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -63,11 +63,11 @@ export const ADVANCED_FEATURE_PRODUCTS = [
 ]
 
 // 矩形屏设备 product 名称列表
-export const RECT_SCREEN_PRODUCTS = ["redmi watch 5", "redmi watch 6", "o65m"]
+export const RECT_SCREEN_PRODUCTS = ["redmi watch 5", "redmi watch 6", "o65m",
+  "emulator-vela"]
 
 // 窄矩形屏设备 product 名称列表
-export const NARROW_RECT_SCREEN_PRODUCTS = ["xiaomi smart band 9 pro",
-  "emulator-vela"]
+export const NARROW_RECT_SCREEN_PRODUCTS = ["xiaomi smart band 9 pro"]
 
 // 圆形屏设备 product 名称列表
 export const CIRCLE_SCREEN_PRODUCTS = [
